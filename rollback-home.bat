4c566c5cd4bf4dbd211e03117567194aada4bd70
Justin G0KSC <justinjohnson256@gmail.com>
2026-09-25 16:11:12 +0100

    k

--- rollback-home.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Rolling back home.blade.php to original version...
-copy /Y "resources\views\pages\home.blade.php.backup" "resources\views\pages\home.blade.php"
-echo.
-echo Rollback complete!
-echo.
-echo To restore the enhanced version, run: restore-enhanced.bat
-pause
